5ace4a9d345b08c67c4580c002a3bddcb4933ff7
Peggy Li <xpeggyxli@gmail.com>
2018-10-20 19:04:58 -0400

    Add user login/registration

--- app/models.py
+++ app/models.py
@@ -1,15 +1,17 @@
-from app import db
+from app import db, login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 class Party(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    main_contact = db.Column(db.Integer, db.ForeignKey('citizen.id'))
+    main_contact = db.Column(db.Integer, db.ForeignKey('user.id'))
     last_contact = db.Column(db.DateTime)
 
     def __repr__(self):
         return '<Party {}>'.format(self.id)
 
 
-class Citizen(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     phone_number = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(128), index=True, unique=True)
@@ -21,7 +23,13 @@ class Citizen(db.Model):
     is_safe = db.Column(db.Boolean)
 
     def __repr__(self):
-        return '<Citizen {}>'.format(self.name)
+        return '<User {}>'.format(self.name)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 class Location(db.Model):
@@ -43,3 +51,8 @@ class PartySupply(db.Model):
     party_id = db.Column(db.Integer, db.ForeignKey('party.id'))
     supply_id = db.Column(db.Integer, db.ForeignKey('supply.id'))
     have = db.Column(db.Boolean)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
